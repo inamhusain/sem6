1f7577ee80e0e7e6cda1fb4ee4308df478d8d01a
inamhusain <inamhusain220@gmail.com>
2021-03-26 11:57:42 +0530

    .

---
+++ ASP.NET/SEM6/P5/P5/WebForm1.aspx
@@ -0,0 +1,37 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="P5.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <center>
+        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+            <br />
+        <asp:UpdatePanel ID="UpdatePanel1" runat="server">
+            <ContentTemplate>
+                &nbsp;
+                <h1><asp:Label ID="lblhr" runat="server" Text="00" ForeColor="Red"></asp:Label></h1> &nbsp;Hour, 
+                <h1><asp:Label ID="lblmin" runat="server" Text="00" ForeColor="Red"></asp:Label></h1> &nbsp;Min,   
+                <h1><asp:Label ID="lblsec" runat="server" Text="00" ForeColor="Red"></asp:Label></h1> &nbsp;Sec. 
+                <asp:Timer ID="Timer1" runat="server" OnTick="ontick" Interval="1000" ></asp:Timer>
+                <br><br>
+                <asp:Button ID="btnStart" runat="server" OnClick="btnStart_Click" Text="Start" />
+                &nbsp;&nbsp;
+                <asp:Button ID="btnStop" runat="server" OnClick="btnStop_Click" Text="Stop" />
+                &nbsp;&nbsp;
+                <asp:Button ID="btnReset" runat="server" OnClick="btnReset_Click" Text="Reset" />
+           
+
+            </ContentTemplate>
+        </asp:UpdatePanel>
+            </center>
+    </div>
+        
+    </form>
+</body>
+</html>
---
+++ ASP.NET/SEM6/P5/P5/WebForm1.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace P5
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        static int counthr = 00;
+        static int countmin = 59;
+        static int countsec = 50;
+        protected void ontick(object sender, EventArgs e)
+        {
+            countsec++;
+            if (countsec > 59)
+            {
+                countmin++;
+                countsec = 00;
+            }
+            else if (countmin > 59)
+            {
+                counthr++;
+                countmin = 00;
+            }
+            lblhr.Text = counthr.ToString();
+            lblmin.Text = countmin.ToString();
+            lblsec.Text = countsec.ToString();
+        }
+        protected void btnStart_Click(object sender, EventArgs e)
+        {
+            Timer1.Enabled = true;
+        }
+
+        protected void btnStop_Click(object sender, EventArgs e)
+        {
+            Timer1.Enabled = false;
+                   
+        }
+
+        protected void btnReset_Click(object sender, EventArgs e)
+        {
+            countsec = 00;
+            countmin = 00;
+            counthr = 00;
+            lblhr.Text = counthr.ToString();
+            lblmin.Text = countmin.ToString();
+            lblsec.Text = countsec.ToString();
+        }
+    }
+}
