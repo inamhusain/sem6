469458d09d8a1d0d2f874cb0346428775c05dbdd
RahemanAli321 <76989478+RahemanAli321@users.noreply.github.com>
2021-03-28 04:32:03 -0700

    Update p3.php

--- PHP/p3.php
+++ PHP/p3.php
@@ -1,13 +1,15 @@
+
 <?php 
-  $no = 1;
   $no1 = 0;
   $no2 = 1;
   echo $no1." ";
+  echo $no2." ";
   for($i=0;$i<10;$i++)
   {
-    echo $no." ";
-    $no = $no1 + $no2;
+ 
+    $no3 = $no1 + $no2;
+    echo $no3." ";
     $no1= $no2;
-    $no2 = $no;
+    $no2 = $no3;
   }
 ?>
